97e8fb9ca1db044427546f2bd21410da51d27c89
UIises Flores <66712163+ulysses-flrss@users.noreply.github.com>
2022-10-13 15:52:48 -0600

    Add files via upload

---
+++ Aplicaciom de web guia/Models/Categorias.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace SitioWebdeSUCARNET.Models
+{
+    public class categorias
+    {
+        public List<SelectListItem> Categorias { get; set; }
+        public int nIdCategoria;
+        public string sNombreCategoria;
+    }
+}
---
+++ Aplicaciom de web guia/Models/Localidad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace SitioWebdeSUCARNET.Models
+{
+    public class Localidad
+    {
+        public List<SelectListItem> Localidades { get; set; }
+        public int nIdMunicipio;
+        public string sNombreMunicipio;
+        public int nIdPais;
+    }
+}
